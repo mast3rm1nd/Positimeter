bfe82b6c88ce271f5047faf2bd5cdae8dbc2b774
mastermind <cyberdude@mail.ru>
2016-04-21 01:45:30 +0300

    +

---
+++ Positimeter/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+
+namespace Positimeter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string[] positiveWordsList = File.ReadAllLines("positive.txt", Encoding.Default).Select(x => x.ToLower()).ToArray();
+        static string[] negativeWordsList = File.ReadAllLines("negative.txt", Encoding.Default).Select(x => x.ToLower()).ToArray();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //var f = TextHelper.GetWordsStatistics("Шла саша по шоссе и сосала хуилу. Саша шла.");
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop)) return;
+            
+            // Note that you can have more than one file.
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files.Count() != 1)
+            {
+                MessageBox.Show("Поддерживается обработка только одного файла.");
+                return;
+            }
+            else
+            {
+                var text = File.ReadAllText(files[0], Encoding.Default);
+
+                Positivity_Label.Content = $"Позитивность: {GetTextPositivity(text)}";
+            }            
+        }
+
+
+        static int GetTextPositivity(string text)
+        {
+            var wordsStatistics = TextHelper.GetWordsStatistics(text);
+
+            var positivity = 0;
+
+            foreach(var pair in wordsStatistics)
+            {
+                if (positiveWordsList.Contains(pair.Key))
+                {
+                    positivity += pair.Value;
+                    continue;
+                }
+                    
+
+                if (negativeWordsList.Contains(pair.Key))
+                    positivity -= pair.Value;
+            }
+
+            return positivity;
+        }
+    }
+}
---
+++ Positimeter/TextHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Positimeter
+{
+    class TextHelper
+    {
+        public static Dictionary<string, int> GetWordsStatistics(string text)
+        {
+            var statistics = new Dictionary<string, int>();
+
+            var isInWord = false;
+
+            var startCopyPos = 0;
+            
+
+            for (int index = 0; index < text.Length; index++)
+            {
+                if(!char.IsLetter(text[index]) && isInWord) // here is word end
+                {
+                    isInWord = false;
+
+                    var word = text.Substring(startCopyPos, index - startCopyPos).ToLower();
+
+                    if (statistics.ContainsKey(word))
+                        statistics[word]++;
+                    else
+                        statistics[word] = 1;
+
+                    continue;                    
+                }
+
+                if(char.IsLetter(text[index]) && !isInWord) // word start
+                {
+                    isInWord = true;
+
+                    startCopyPos = index;
+                }
+            }
+
+            return statistics.OrderByDescending(x => x.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+        }
+    }
+}
